039375cd6ddc9baa8b678bb86be60800bbd4d7ee
Abby Brown <abby_brown3@baylor.edu>
2021-04-29 18:52:40 -0500

    added, migrated, and registered Topping model

--- pizzas/models.py
+++ pizzas/models.py
@@ -2,13 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Pizza(models.Model):
-    pizza_type = models.CharField(max_length=150)
+    pizza_name = models.CharField(max_length=150)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.pizza_type
-
-    
+        return self.pizza_name
 
 
-"""class Toppings():"""
+class Topping(models.Model):
+    pizza_name = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    topping_name = models.CharField(max_length=150)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "toppings"
+        # Okay, this is important so the plural is right? Not totally sure.
+
+    def __str__(self):
+        return self.topping_name

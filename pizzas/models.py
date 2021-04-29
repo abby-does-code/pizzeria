ef8a88980e8e0f1052de024caed016495843c6ef
Abby Brown <abby_brown3@baylor.edu>
2021-04-29 18:28:58 -0500

    working!

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Pizza(models.Model):
+    pizza_type = models.CharField(max_length=150)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.pizza_type

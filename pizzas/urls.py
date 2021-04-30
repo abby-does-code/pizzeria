5efbcb097ecad7564439db11bdf3c39b57ff1bee
Abby Brown <abby_brown3@baylor.edu>
2021-04-30 08:29:25 -0500

    preparing to create web pages

---
+++ pizzas/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+# Dot tells Python to import the views.py modeul from the same directory
+# as the current urls module
+from . import views
+
+# Variable app_name helps Django distinguish this urls.py file from
+# files of the same name in other apps within the project
+app_name = "pizzas"
+
+urlpatterns = [
+    # Notes: the first argument is empty string which matches the base URL
+    ## The second arugment specifies the funciton name to call in views.
+    ### Third argument provides the name 'index' for this URL to refer to later
+    path("", views.index, name="index")
+]

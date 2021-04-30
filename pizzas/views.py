4b5a7589b90a276c1c3244198e17551180e94b5a
Abby Brown <abby_brown3@baylor.edu>
2021-04-30 08:42:19 -0500

    added index and base html; updated index to inherit from base

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+# When URL request matches the pattern just defined,
+# Django looks for a function called index() in the views.py file
+
+# View function takes in information from a request, prepares data needed,
+# and then sends the data back to the browser; using template that defines what the page
+# looks like.
+
+
+def index(request):
+    """Home page"""
+    return render(request, "pizzas/index.html")
